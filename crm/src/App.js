c0a84356f05bcb90c0d01a9297f48e3f74b8964e
DavidWz0403 <52293898+DavidWz0403@users.noreply.github.com>
2021-05-18 10:28:48 +0200

    Footer header main and layout created

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import { Button } from 'react-bootstrap';
 import Entry from './page/entry/entry.page';
-
+import DefaultLayout from './layout/DefaultLayout';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+
+      <DefaultLayout>
+        send page content
+      </DefaultLayout>
     </div>
   );
 }
---
+++ crm/src/layout/partials/Header.comp.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../assets/img/logo.png';
+import './header.css';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect
+            bg="info"
+            variant="dark"
+            expand="md">
+            <Navbar.Brand>
+                <img src={logo} alt="logo" width="50px" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link href="/dashboard">Tickets</Nav.Link>
+                    <Nav.Link href="/dashboard">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+
+        </Navbar>
+
+
+    )
+}
+
+export default Header
